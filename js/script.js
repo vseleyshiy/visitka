e10a7cf8374851ba0ef748b77cd7f266c80c94b9
vseleyshiy <122272280+vseleyshiy@users.noreply.github.com>
2023-11-08 20:45:57 +0500

    доработал слайдер и добавил описание к проектам

--- js/script.js
+++ js/script.js
@@ -1,25 +1,88 @@
-let offset = 0; //смещение от левого края
+let offset = 0;
 const slider = document.querySelector('.slider__list');
 btnNext = document.querySelector('.slider__button-next');
 btnPrev = document.querySelector('.slider__button-prev');
+let dots = document.getElementById('dots');
+let dot1 = document.getElementById('dot1');
+let dot2 = document.getElementById('dot2');
 
 btnNext.addEventListener('click', function () {
-    offset = offset + 290;
-    slider.style.left = -offset + 'px';
+    offset = offset + 265;
+    slider.style.transform = 'translateX(' + -offset + 'px)';
     if (offset > 0) {
-        offset = -290;
+        offset = -265;
+    };
+    if (dot2.classList.contains('slider__dot-active')) {
+        dot2.classList.remove('slider__dot-active');
+        dot1.classList.add('slider__dot-active');
+    } else {
+        dot1.classList.remove('slider__dot-active');
+        dot2.classList.add('slider__dot-active');
     };
 });
 
 function time() {
-    offset = offset + 290;
-    slider.style.left = -offset + 'px';
+    offset = offset + 265;
+    slider.style.transform = 'translateX(' + -offset + 'px)';
     if (offset > 0) {
-        offset = -290;
+        offset = -265;
+    };
+    if (dot2.classList.contains('slider__dot-active')) {
+        dot2.classList.remove('slider__dot-active');
+        dot1.classList.add('slider__dot-active');
+    } else {
+        dot1.classList.remove('slider__dot-active');
+        dot2.classList.add('slider__dot-active');
     };
 };
 
-setInterval(time, 11000)
+setInterval(time, 11000);
+
+
+
+let info1 = document.getElementById('info1');
+let info2 = document.getElementById('info2');
+let info3 = document.getElementById('info3');
+let infoButton1 = document.getElementById('infoButton1');
+let infoButton2 = document.getElementById('infoButton2');
+let infoButton3 = document.getElementById('infoButton3');
+let infoButtonHide1 = document.getElementById('infoButtonHide1');
+let infoButtonHide2 = document.getElementById('infoButtonHide2');
+let infoButtonHide3 = document.getElementById('infoButtonHide3');
+
+
+infoButton1.addEventListener('click', function () {
+    info1.style.display = 'block';
+    infoButtonHide1.style.display = 'block';
+    infoButton1.style.display = 'none';
+});
+infoButtonHide1.addEventListener('click', function () {
+    info1.style.display = 'none';
+    infoButtonHide1.style.display = 'none';
+    infoButton1.style.display = 'block';
+});
+
+infoButton2.addEventListener('click', function () {
+    info2.style.display = 'block';
+    infoButtonHide2.style.display = 'block';
+    infoButton2.style.display = 'none';
+});
+infoButtonHide2.addEventListener('click', function () {
+    info2.style.display = 'none';
+    infoButtonHide2.style.display = 'none';
+    infoButton2.style.display = 'block';
+});
+
+infoButton3.addEventListener('click', function () {
+    info3.style.display = 'block';
+    infoButtonHide3.style.display = 'block';
+    infoButton3.style.display = 'none';
+});
+infoButtonHide3.addEventListener('click', function () {
+    info3.style.display = 'none';
+    infoButtonHide3.style.display = 'none';
+    infoButton3.style.display = 'block';
+});
 // const images = document.querySelectorAll('.slider__item');
 // const controlls = document.querySelectorAll('.slider__button');
 // let ImageIndex = 1;
