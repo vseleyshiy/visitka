7f8779c09dff388340ccd832f2da7c31e20a4aa8
vseleyshiy <122272280+vseleyshiy@users.noreply.github.com>
2024-11-30 17:47:09 +0500

    Another fixes for optimisation.

---
+++ js/database.js
@@ -0,0 +1,82 @@
+export const projects = [
+	{
+		link: 'https://github.com/vseleyshiy/EasyLinks',
+		github_link: 'https://github.com/vseleyshiy/EasyLinks',
+		title: '&#9830; [CLICK] Web App on Python & eel | EasyLinks &#9830;',
+		image: 'img/easylinks_img.webp',
+		text_title:
+			'Приложение, которое поможет вам удобно открывать большое кол-во ссылок в браузере',
+		description: `
+        <span>
+            Подробная инструкция на английском есть на GitHub главной странице проекта в README файле.
+        </span>
+        <span>
+            Ссылка на неё ниже.
+        </span>
+        `,
+	},
+	{
+		link: 'https://github.com/vseleyshiy/ReactMakimaCoin',
+		github_link: 'https://github.com/vseleyshiy/ReactMakimaCoin',
+		title: '&#9824; [CLICK] Game | ReactMakimaCoin &#9824;',
+		image: 'img/reactmakimacoin_img.webp',
+		text_title: 'Копия Hamster Kombat (старого) на React JS + PHP с Макимой <3',
+		description: `
+        <span>Сделана главная секция, секция с улучшениями, которая работает,
+        но после перезагрузки страницы изменения пропадают.
+        Так же сделана секция с ссылками, за подписку дается монета и тд)</span>
+        <span>Работой доволен, при помощи нее изучил библиотеку React JS, в будущем сделаю Backend на PHP.</span>
+        <span>
+        <b>Upd:</b> Back-end на PHP сделан, но сделан он конечно не очень. Я совсем не работал с cookie и сессиями + я не знал как вообще правильно пишется back-end на React, поэтому делал всё по своему. В общем, это мой первый опыт работы с back-end на React + он на PHP, поэтому для первого раза очень даже хорошо. А обо всех минусах я и сам знаю) <3
+        </span>
+        `,
+	},
+	{
+		link: 'https://vseleyshiy.github.io/cheerful-gardener/',
+		github_link: 'https://github.com/vseleyshiy/cheerful-gardener',
+		title: '&#9827; [CLICK] Game | cheerful gardener &#9827;',
+		image: 'img/cheerfulgardener_img.webp',
+		text_title: 'Веселый фермер фармилка',
+		description: `<span>Фармишь на грядки, покупаешь их, фармишь удобрения, удобряешь грядки.
+        Пока не обновишь страницу, прогресс не пропадает.</span>`,
+	},
+]
+
+export const stacks = [
+	{
+		title: 'HTML',
+		img: 'img/stacks/html.svg',
+	},
+	{
+		title: 'CSS',
+		img: 'img/stacks/css.svg',
+	},
+	{
+		title: 'SCSS',
+		img: 'img/stacks/scss.svg',
+	},
+	{
+		title: 'JavaScript',
+		img: 'img/stacks/js.svg',
+	},
+	{
+		title: 'TypeScript',
+		img: 'img/stacks/ts.svg',
+	},
+	{
+		title: 'Vite React',
+		img: 'img/stacks/vitereact.svg',
+	},
+	{
+		title: 'PHP',
+		img: 'img/stacks/php.svg',
+	},
+	{
+		title: 'MySQL',
+		img: 'img/stacks/mysql.svg',
+	},
+	{
+		title: 'Axios',
+		img: 'img/stacks/axios.svg',
+	},
+]
--- js/script.js
+++ js/script.js
@@ -1,58 +1,17 @@
-// Database
-const projects = [
-    {
-        link: 'https://github.com/vseleyshiy/EasyLinks',
-        github_link: 'https://github.com/vseleyshiy/EasyLinks',
-        title: '&#9830; [CLICK] Web App on Python & eel | EasyLinks &#9830;',
-        image: 'img/easylinks_img.jpg',
-        text_title: 'Приложение, которое поможет вам удобно открывать большое кол-во ссылок в браузере',
-        description: `
-        <span>
-            Подробная инструкция на английском есть на GitHub главной странице проекта в README файле.
-        </span>
-        <span>
-            Ссылка на неё ниже.
-        </span>
-        `,
-    },
-    {
-        link: 'https://github.com/vseleyshiy/ReactMakimaCoin',
-        github_link: 'https://github.com/vseleyshiy/ReactMakimaCoin',
-        title: '&#9824; [CLICK] Frontend Lending | ReactMakimaCoin &#9824;',
-        image: 'img/reactmakimacoin_img.jpg',
-        text_title: 'Фронтенд копия Hamster Kombat (старого) на React JS с Макимой <3',
-        description: `
-        <span>Сделана главная секция, секция с улучшениями, которая работает,
-        но после перезагрузки страницы изменения пропадают.
-        Так же сделана секция с ссылками, за подписку дается монета и тд)</span>
-        <span>Работой доволен, при помощи нее изучил библиотеку React JS, в будущем сделаю Backend на PHP.</span>
-        `,
-    },
-    {
-        link: 'https://vseleyshiy.github.io/cheerful-gardener/',
-        github_link: 'https://github.com/vseleyshiy/cheerful-gardener',
-        title: '&#9827; [CLICK] Игра | cheerful gardener &#9827;',
-        image: 'img/cheerfulgardener_img.jpg',
-        text_title: 'Веселый фермер фармилка',
-        description: `<span>Фармишь на грядки, покупаешь их, фармишь удобрения, удобряешь грядки.
-        Пока не обновишь страницу, прогресс не пропадает.</span>`,
-    },
-]
+import { projects, stacks } from './database.js'
 
-// Code
-
-const modal = document.querySelector('.modal');
-const modal__close = document.getElementById('modal__close');
+const modal = document.querySelector('.modal')
+const modal__close = document.getElementById('modal__close')
 
 modal.addEventListener('close', () => {
-    document.body.classList.remove('lock');
+	document.body.classList.remove('lock')
 })
 
-const list = document.querySelector('.hero__list');
+const list = document.querySelector('.hero__list')
 
 function get_projects() {
-    projects.forEach(el => {
-        const item = `
+	projects.forEach(el => {
+		const item = `
         <div class="hero__item" style="background-image: url(${el.image})" target="_blank">
             <div class="hero__item-info">
                 <a href="${el.link}" class="hero__item-title" target="_blank">
@@ -62,30 +21,30 @@ function get_projects() {
                     Подробнее
                 </div>
             </div>
-        </div>`;
-        list.innerHTML += item;
-    });
-    const btns = document.querySelectorAll('.hero__item-btn');
+        </div>`
+		list.innerHTML += item
+	})
+	const btns = document.querySelectorAll('.hero__item-btn')
 
-    btns.forEach((el, index) => {
-        el.addEventListener('click', () => {
-            get_description(index);
-            modal.showModal();
-            document.body.classList.add('lock');
-        });
-    });
+	btns.forEach((el, index) => {
+		el.addEventListener('click', () => {
+			get_description(index)
+			modal.showModal()
+			document.body.classList.add('lock')
+		})
+	})
 }
 
-get_projects();
+get_projects()
 
 function get_description(index) {
-    let title;
-    if (projects[index].title.includes('[CLICK]')) {
-        title = projects[index].title.replace('[CLICK]', '');
-    } else {
-        title = projects[index].title;
-    }
-    const item = `
+	let title
+	if (projects[index].title.includes('[CLICK]')) {
+		title = projects[index].title.replace('[CLICK]', '')
+	} else {
+		title = projects[index].title
+	}
+	const item = `
     <div class="modal__img" style="background-image: url(${projects[index].image})">
         <div class="modal__title">
             ${title}
@@ -103,6 +62,27 @@ function get_description(index) {
         </a>
     </div>
     `
-    const modal__content = document.querySelector('.modal__content');
-    modal__content.innerHTML = item;
+	const modal__content = document.querySelector('.modal__content')
+	modal__content.innerHTML = item
 }
+
+const stacksList = document.querySelector('.stacks')
+
+stacks.forEach((el, index) => {
+	const item = `
+        <div class="stack__item">
+                <div class="stack__img-wrap">
+                    <img
+                        src="${el.img}"
+                        alt="${el.title}"
+                        class="stack__img"
+                    />
+                </div>
+            <div class="stack__title">${el.title}</div>
+        </div>`
+	stacksList.innerHTML += item
+	if (index == stacks.length - 1) {
+		stacksList.innerHTML +=
+			'<span style="color: #8c8c8c;font-size: 12px;">и прочие...</span>'
+	}
+})
